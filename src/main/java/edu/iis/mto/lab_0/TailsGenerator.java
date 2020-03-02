a81d77bef98e7611eccce4ee51ae2a7fa138f9e3
MichalOgloza <michalogloza97@gmail.com>
2020-03-02 18:21:02 +0100

    Implemented tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        List<String> lst = new ArrayList<>();
+
+        for(int i = 0; i <= value.length(); i++)
+            lst.add(value.substring(i));
+        return lst;
     }
 
 }
