4e895c81c52d3e6054502429a49bbb5ed3268ca6
MichalOgloza <michalogloza97@gmail.com>
2020-03-02 18:41:17 +0100

    fixed formatting

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -8,8 +8,9 @@ public class TailsGenerator {
     public List<String> tails(String value) {
         List<String> tailList = new ArrayList<>();
 
-        for(int i = 0; i <= value.length(); i++)
+        for (int i = 0; i <= value.length(); i++) {
             tailList.add(value.substring(i));
+        }
         return tailList;
     }
 
